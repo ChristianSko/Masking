4bc48d6b336181e9771b1d2cc8174d49b9adef4d
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-04 09:20:03 +0200

    Initial Commit

---
+++ Masking/MaskingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaskingApp.swift
+//  Masking
+//
+//  Created by Skorobogatow, Christian on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MaskingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
